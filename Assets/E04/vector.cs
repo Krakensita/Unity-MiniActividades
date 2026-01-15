9db1fbc78b072715f6ccd70e9f166097c2e53124
Krakensita <eloi.rosa@estudiant.enti.cat>
2026-01-15 11:06:29 +0100

    partes

---
+++ Assets/E04/vector.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class vector : MonoBehaviour
+{
+    public Vector3 num;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log(num);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
